9c23d6e75b437548ab77f2c2525f3c23ca6ad094
Koushik Siva <koushiksiva9@gmail.com>
2024-04-14 21:04:05 -0500

    Added the frontend link in cors policy

--- smarthealth/Program.cs
+++ smarthealth/Program.cs
@@ -21,6 +21,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 builder.Services.AddCors(p=>p.AddPolicy("corspolicy",build =>
 {
     build.WithOrigins("http://localhost:3000").AllowAnyMethod().AllowAnyHeader();
+    build.WithOrigins("https://react-smarthealth.vercel.app/").AllowAnyMethod().AllowAnyHeader();
 }));
 
 builder.Services.AddMetricServer(options =>
