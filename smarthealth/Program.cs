554a475fabeb25b0f915a665294c40cc63cc145d
Koushik Siva <koushiksiva9@gmail.com>
2024-04-14 20:29:26 -0500

    Updated the swagger settings

--- smarthealth/Program.cs
+++ smarthealth/Program.cs
@@ -59,11 +59,10 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 app.UseCors("corspolicy");
 app.UseHttpsRedirection();
